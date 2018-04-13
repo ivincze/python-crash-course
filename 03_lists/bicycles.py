68aef53e996de4620ba01df556a38233e5e7b8fc
Vinnie <37813261+vinczeim@users.noreply.github.com>
2018-04-13 14:34:47 +0200

    Create bicycles.py

---
+++ 03_lists/bicycles.py
@@ -0,0 +1,15 @@
+# List:
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# 0-based indexing:
+print(bicycles[0])
+print(bicycles[1])
+print(bicycles[3])
+
+print(bicycles[0].title())
+
+# Indexing from end of the list:
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
